6e28acf90de7790fdc7aaf868e18eb8505d06797
FleshHerbal <34520508+FleshHerbal@users.noreply.github.com>
2022-10-19 08:23:04 +0500

    Add files via upload

---
+++ dataBaseService.js
@@ -0,0 +1,82 @@
+import sqlite3 from "sqlite3";
+
+export default class DataBaseService{
+    constructor(query = "", type = types.run){
+        this._query = query;
+        this._type = type;
+        this._dbPath = "./dictionary.db";
+        this._createTables();
+    } 
+	
+    _createTables(){
+        const querys = []
+        //querys.push("CREATE TABLE IF NOT EXISTS YOUR_TABLE (id INTEGER PRIMARY KEY AUTOINCREMENT, ...)");
+
+        querys.forEach(element => {
+            const db = new sqlite3.Database(this._dbPath, (err) => {
+                if(err) console.log(`Error created table: ${err.message}`);
+                db.run(element, (err) => {
+                    if(err) console.log(`Error add create table: ${err.message}`);
+                });
+            })
+        });
+    }
+
+    _runTransaction(arrayData){
+        return new Promise((resolve, reject) => {
+            const db = new sqlite3.Database(this._dbPath, (err) => {
+                if(err) reject(err.message);
+                db.run(this._query, arrayData, (err)=>{
+                    if(err) reject(err.message);
+                    resolve("ok");
+                })
+            })
+        })
+    }
+
+    _allTransaction(){
+
+        return new Promise((resolve, reject) => {
+            const db = new sqlite3.Database(this._dbPath, (err) => {
+                if(err) reject(err.message);
+                db.all(this._query, (err, rows) => {
+                    if(err) reject(err.message);
+                    resolve(rows);
+                })
+            })
+        })
+    }
+
+    _getTransaction(){
+        return new Promise((resolve, reject) => {
+            const db = new sqlite3.Database(this._dbPath, (err) => {
+                if(err) reject(err.message);
+                db.get(this._query, (err, row) => {
+                    if(err) reject(err.message);
+                    resolve(row);
+                })
+            })
+        })
+    }
+
+    startTransaction(arrayData = []){
+        switch (this._type) {
+            case types.all:
+                return this._allTransaction();
+            case types.get:
+                return this._getTransaction();
+            case types.run:
+                return this._runTransaction(arrayData);        
+            default:
+                return new Promise((_, reject) => reject("type not select"));
+        }
+    }
+
+}
+
+//Specifies the type of transaction
+export const types = {
+    all: "a0mSdk23sd",
+    get: "qNwAdlw321a",
+    run: "h78eIa1Nv"
+}
